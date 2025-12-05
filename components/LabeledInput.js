e08ec2756c62a42f544a5c8b692d4255adf9c81b
Fitsum <fitsum.asgedom@georgebrown.ca>
2025-12-05 21:44:33 +0100

    creating syles is done

--- components/LabeledInput.js
+++ components/LabeledInput.js
@@ -20,3 +20,18 @@ export default function LabeledInput({
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 12,
+  },
+  label: {
+    fontWeight: "600",
+    marginBottom: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 6,
+    padding: 8,
+  },
+});
